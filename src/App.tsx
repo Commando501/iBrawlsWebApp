e5d3637d0cb1f83bfa522b6a592be731e57eeb71
ASpence <32424441+Commando501@users.noreply.github.com>
2026-05-28 14:23:29 -0500

    Force cloudflare to update

--- src/App.tsx
+++ src/App.tsx
@@ -58,7 +58,7 @@ import { ChatOverlay, ChatMessage } from './components/ChatOverlay';
 import { CharacterPreview } from './components/CharacterPreview';
 import { CharacterLoadout, DEFAULT_LOADOUT, AVAILABLE_PRESETS, HelmetPreset, TorsoPreset, ArmPreset, LegPreset } from './components/VoxelModels';
 
-const APP_VERSION = '0.428';
+const APP_VERSION = '0.428a';
 const MAX_PLAYER_NAME_LENGTH = 10;
 
 interface OnlineClient {
